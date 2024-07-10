3a11a78bdf9065a019de7121041118010ff07611
vanuzita <vanu.mantovani@gmail.com>
2024-07-10 18:52:15 -0300

    Realizar cambios en el formulario

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document.getElementById('contrast').addEventListener('click', function() {
-    document.body.classList.toggle('high-contrast');
-});
+    var button = document.getElementById('contrast');
+    var body = document.body;
 
+    body.classList.toggle('high-contrast');
+
+    if (body.classList.contains('high-contrast')) {
+        button.textContent = 'Modo Claro';
+    } else {
+        button.textContent = 'Modo Oscuro';
+    }
+});
